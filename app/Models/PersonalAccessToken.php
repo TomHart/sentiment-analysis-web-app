2724f4465dc10a7cbbaf3aa93e00b67831dc6537
Tom Hart <thomas.hart@sky.uk>
2021-05-08 18:46:56 +0100

    Added token docblock

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -3,12 +3,43 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\PersonalAccessToken as BaseToken;
 
 /**
  * Class Token
+ *
  * @package App\Models
+ * @property int $id
+ * @property string $tokenable_type
+ * @property int $tokenable_id
+ * @property string $name
+ * @property string $token
+ * @property array|null $abilities
+ * @property int $brain_id
+ * @property Carbon|null $last_used_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Brain $brain
+ * @property-read Model|Eloquent $tokenable
+ * @method static Builder|PersonalAccessToken newModelQuery()
+ * @method static Builder|PersonalAccessToken newQuery()
+ * @method static Builder|PersonalAccessToken query()
+ * @method static Builder|PersonalAccessToken whereAbilities($value)
+ * @method static Builder|PersonalAccessToken whereBrainId($value)
+ * @method static Builder|PersonalAccessToken whereCreatedAt($value)
+ * @method static Builder|PersonalAccessToken whereId($value)
+ * @method static Builder|PersonalAccessToken whereLastUsedAt($value)
+ * @method static Builder|PersonalAccessToken whereName($value)
+ * @method static Builder|PersonalAccessToken whereToken($value)
+ * @method static Builder|PersonalAccessToken whereTokenableId($value)
+ * @method static Builder|PersonalAccessToken whereTokenableType($value)
+ * @method static Builder|PersonalAccessToken whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class PersonalAccessToken extends BaseToken
 {
